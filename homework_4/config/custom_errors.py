de31369670caccc92d803259ad3a8bd09dc17a3b
Mykyta Serhiienko <mykyta.serhiienko@globallogic.com>
2022-04-18 14:11:45 +0300

    Add homework_4 dir, create requests to the app.cosmosid.com

---
+++ homework_4/config/custom_errors.py
@@ -0,0 +1,2 @@
+class AccessTokenIsMissed(Exception):
+    pass
---
+++ homework_4/main.py
@@ -0,0 +1,25 @@
+import requests
+import os
+import logging
+from config.links import LOGIN_LINK, ROOT_FOLDER_LINK
+from config.custom_errors import AccessTokenIsMissed
+
+
+def login():
+    response_ = requests.post(url=LOGIN_LINK,
+                              headers={'Authorization': os.environ.get('AUTHORIZATION_HEADER')})
+    token = response_.json().get('token')
+    if token is None:
+        logging.error('Cannot get token from login request.')
+        raise AccessTokenIsMissed('Cannot get token from login request.')
+    return token
+
+
+def root_folder(token):
+    response_ = requests.get(ROOT_FOLDER_LINK, headers={'x-token': token})
+    logging.warning(response_.text)
+
+
+if __name__ == '__main__':
+    token = login()
+    root_folder(token)
