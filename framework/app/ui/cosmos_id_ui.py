e296a3d26649348a73af7c3242f3f8c436b1b4ce
sergii-butenko-gl <69191372+sergii-butenko-gl@users.noreply.github.com>
2022-05-05 11:03:30 +0300

    Update cosmos_id_ui.py

--- framework/app/ui/cosmos_id_ui.py
+++ framework/app/ui/cosmos_id_ui.py
@@ -4,5 +4,6 @@ from app.ui.page_objects.login_page import LoginPage
 class CosmosIDUI:
     """Class defining web app"""
 
-    def __init__(self, driver):
+    def __init__(self, browser):
+        sefl.driver = BrowsersProvider.get_browser(browser)
         self.login_page = LoginPage(driver)
