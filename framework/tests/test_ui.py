a7e22334a186abc35708b6660a1929c337aa7d57
Mykyta Serhiienko <mykyta.serhiienko@globallogic.com>
2022-05-06 11:35:29 +0300

    update requirements.txt to strong equal ==

--- framework/tests/test_ui.py
+++ framework/tests/test_ui.py
@@ -1,9 +1,9 @@
 def test_login_failed(ui_app):
     """Test user can't login with wrong credentials"""
     ui_app.login_page.login("wrong_email@gmail.com", "wrong_pass")
-    ui_app.login_page.assert_login_failed()
+    assert ui_app.login_page.assert_login_failed() is True
 
 
 def test_sign_up_link(ui_app):
     """Test for check that sign_up link is present on the login page"""
-    ui_app.login_page.assert_sign_up_link_is_present()
+    assert ui_app.login_page.assert_sign_up_link_is_present() is True
